00c65c9db7209116d904f130ddf68938c4c804d2
Jacob Roy <lejacobroy@gmail.com>
2018-08-25 13:58:45 -0400

    Fix a few items in the script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo 'Locale updated to en_US.UTF-8'
 ##Changing Hostname
 CURRNET_HOSTNAME='cat /etc/hostname | tr -d " \t\n\r"'
 rm /etc/hostname
-read -p 'Hostname: ' hostname
+read -p 'Hostname: ' $hostname
 cat $hostname > /etc/hostname
 sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$hostname/g" /etc/hosts
 echo 'Hostname Changed to ' $hostname
@@ -16,12 +16,12 @@ passwd pi &&
 echo 'pi password changed'
 
 #Installing updates and pkg
-apt update
-apt upgrade
-apt dist-upgrade
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
 wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v.last.sh | bash
 echo 'updates and Node.js have been installed'
 
 #Cloning Barberry
-git clone https://github.com/lejacobroy/barberry.git /home/pi/barberry/
+git clone https://github.com/lejacobroy/barberry /home/pi/barberry/
 #cron jobs
